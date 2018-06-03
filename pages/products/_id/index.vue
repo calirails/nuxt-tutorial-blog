28d275f45b1d9fcc3c366227f12db2368ce23104
calvin <calvin98115@yahoo.com>
2018-06-03 15:09:01 -0700

    assignment #1

---
+++ pages/products/_id/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Details for Product ID: {{$route.params.id}}
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'ProductDetail'
+}
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <input type='text' v-model='productId'/>
+        <button v-on:click='loadProduct'>Load Product</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Products',
+    data() {
+        return {
+            productId: null
+        }
+    },
+    methods: {
+        loadProduct() {
+            this.$router.push(`/products/${this.productId}`);
+        }
+    }
+}
+</script>
+
+
