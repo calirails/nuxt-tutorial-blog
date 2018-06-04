fb839de20f5b0b47cc03b0a9a9588a6dd22a655e
calvin <calvin98115@yahoo.com>
2018-06-03 22:08:21 -0700

    fixed product details paths on /products route

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,6 +2,11 @@
     <div>
         <input type='text' v-model='productId'/>
         <button v-on:click='loadProduct'>Load Product</button>
+        <ul>
+            <li v-for='framework in frameworks' v-bind:key='framework.id'>
+                <nuxt-link v-bind:to="'/products/' + framework.id">{{framework.desc}}</nuxt-link>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -10,7 +15,16 @@ export default {
     name: 'Products',
     data() {
         return {
-            productId: null
+            productId: null,
+            frameworks: [
+                {id: 1, desc: 'VueJS'},
+                {id: 2, desc: 'Nuxt'}
+            ]
+        }
+    },
+    computed: {
+        pathForProductDetails(framework) {
+            return `/products/${framework.desc}`;
         }
     },
     methods: {
